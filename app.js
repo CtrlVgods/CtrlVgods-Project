f9a6acf100bd47aafe9fc41deb2859c2139b1a07
Stan T <86654387+Twalinzor@users.noreply.github.com>
2022-02-18 17:07:24 +0100

    Auth route test

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-require('dotenv').config()
+require("dotenv").config();
 
-var createError = require('http-errors');
-var express = require('express');
+var createError = require("http-errors");
+var express = require("express");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const authRoutes = require("./routes/auth");
 
 var app = express();
 
 // Functional curling style of loading configuration
-require('./config/db')
-require('./config/global')(app)
+require("./config/db");
+require("./config/global")(app);
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/auth", authRoutes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
