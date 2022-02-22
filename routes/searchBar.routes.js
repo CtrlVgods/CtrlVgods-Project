dd2e9efa87f88758d30cda7b0b52d0e972397f44
Daniel Milan <danielmilanvera@gmail.com>
2022-02-22 11:08:33 +0100

    .

--- routes/searchBar.routes.js
+++ routes/searchBar.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
+
 const GamesApi = require("../services/gamesApi")
 const gamesApiHandler = new GamesApi()
 
