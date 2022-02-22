9e951fb146378505f3890e09fbbf778a54a1f544
Daniel Milan <danielmilanvera@gmail.com>
2022-02-22 13:05:57 +0100

    Boodstrap

--- routes/searchBar.routes.js
+++ routes/searchBar.routes.js
@@ -1,10 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const Game = require("../models/Game.model");
+
+const isLoggedIn = require("../middleware/isLoggedIn");
+const GamesApi = require("../services/gamesApi");
+const gamesApiHandler = new GamesApi();
+const Review = require("../models/Review.model");
+const User = require("../models/User.model");
 
 
-const GamesApi = require("../services/gamesApi")
-const gamesApiHandler = new GamesApi()
+router.post("/gameName", (req, res)=>{
+
+    const gameName = req.body.gameName
+    
+    Game.findOne({title:gameName})
+        .then((game) => {
+            
+            res.redirect(`/games/${game.id}/details`)
+        })
+})
+
+    
+
+
+
+
 
 
 
